8494d6b3750474cd81e120a7938e901f7a0a2c53
shakenokirimi12 <ken20051205@gmail.com>
2024-04-26 03:43:01 +0900

    getを軽く実装

--- src/modules/stores/stores_get.mjs
+++ src/modules/stores/stores_get.mjs
@@ -1,9 +1,20 @@
 import { headerBuilder } from "../headerBuilder.mjs"
 
 export async function handle_GET_Stores(request, env) {
-    let testresponsejson = {
-        "message": "Success!",
-        "status_Code": "NO-S-T01"
+    const { searchParams } = new URL(request.url)
+    let name = searchParams.get('name');
+    let name_start = searchParams.get('name_start');
+    let name_end = searchParams.get('name_end');
+    let price_max = searchParams.get('price_max');
+    let price_min = searchParams.get('price_min');
+    let store_name = searchParams.get('store_name');
+    let store_id = searchParams.get('store_id');
+    if (searchParams.has('geolocation_lat') && searchParams.has('geolocation_lon')) {
+        let geolocation_lat = searchParams.get('geolocation_lat');
+        let geolocation_lon = searchParams.get('geolocation_lon');
+    }
+    else {
+        return new Response(JSON.stringify([{ "message": "You can't get values with only latitude or only longtitude.", "status_Code": "ST-CE-M11" }]), await headerBuilder(200))
     }
     return new Response(JSON.stringify(testresponsejson), await headerBuilder(200))
 }
